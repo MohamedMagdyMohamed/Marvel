976790ca123ccf52c480fdaf0daa366a0e8608f1
MohamedMagdyMohamed <mm.medany96@gmail.com>
2021-05-30 23:42:37 +0200

    Add Navigation Component and empty HomeFragment

--- app/src/main/java/com/example/marvel/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/marvel/ui/activities/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.marvel.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.marvel.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        configureNavController()
+    }
+
+    private fun configureNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_nav_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/example/marvel/ui/home/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.example.marvel.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.marvel.databinding.FragmentHomeBinding
+import com.example.marvel.utils.autoCleared
+
+class HomeFragment : Fragment() {
+
+    private var binding by autoCleared<FragmentHomeBinding>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val dataBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = dataBinding
+        return binding.root
+    }
+}
